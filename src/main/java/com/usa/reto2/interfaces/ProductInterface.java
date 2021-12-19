b2d517273029c87359c5d463a61b4a38796681f9
Deivy Rodriguez <deivy.rodriguez.mt@correo.usa.edo.co>
2021-12-19 12:33:43 -0500

    ajuste cosmetics

--- src/main/java/com/usa/reto2/interfaces/ProductInterface.java
+++
@@ -1,8 +0,0 @@
-package com.usa.reto2.interfaces;
-
-import com.usa.reto2.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductInterface extends MongoRepository<Product, String> {
-
-}
--- src/main/java/com/usa/reto2/model/Cosmetics.java
+++ src/main/java/com/usa/reto2/model/Cosmetics.java
@@ -7,10 +7,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
-@Document(collection = "products")
+@Document(collection = "cosmetics")
 @Data
 @NoArgsConstructor
-public class Product {
+public class Cosmetics {
     @Id
     private String reference;
     private String brand;
